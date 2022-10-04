bfc75159d6ca1bbc85a8d8f6689a1000c2e627a6
Matt Solomon <matt@mattsolomon.dev>
2022-10-03 17:22:13 -0700

    style: forge fmt + named imports

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+  uint public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+  function setNumber(uint newNumber) public {
+    number = newNumber;
+  }
 
-    function increment() public {
-        number++;
-    }
+  function increment() public {
+    number++;
+  }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import {Counter} from "src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+  Counter public counter;
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
+  function setUp() public {
+    counter = new Counter();
+    counter.setNumber(0);
+  }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+  function testIncrement() public {
+    counter.increment();
+    assertEq(counter.number(), 1);
+  }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+  function testSetNumber(uint x) public {
+    counter.setNumber(x);
+    assertEq(counter.number(), x);
+  }
 }
