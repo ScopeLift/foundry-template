a11fbc9fe6e3f756dbdb76a3b34fefa0b9dd0b01
Matt Solomon <matt@mattsolomon.dev>
2022-10-07 12:47:06 -0700

    chore: exclude slither detector and fix test names (#3)
    * one test should error
    
    * chore: fix naming convention + exclude that slither check

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -12,12 +12,12 @@ contract CounterTest is Test {
     counter.setNumber(0);
   }
 
-  function testIncrement() public {
+  function test_Increment() public {
     counter.increment();
     assertEq(counter.number(), 1);
   }
 
-  function testSetNumber(uint x) public {
+  function test_SetNumber(uint x) public {
     counter.setNumber(x);
     assertEq(counter.number(), x);
   }
