61ac5d794e3d7ee80af00f395ca5a8e54528cea8
Ben DiFrancesco <ben@scopelift.co>
2024-06-03 10:41:56 -0400

    Bump Solidity to v 0.8.26 & forge-std to v1.8.2

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.23;
+pragma solidity 0.8.26;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Deploy} from "script/Deploy.s.sol";
