5c7279cd6d233a5aaef2793e31f625ca8d57ec4c
Ben DiFrancesco <ben@scopelift.co>
2025-03-04 09:01:07 -0500

    Bump to solc v0.8.28 and forge-std v1.9.6

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
+pragma solidity 0.8.28;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Deploy} from "script/Deploy.s.sol";
